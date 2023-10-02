c511eefbd9390dab3adbc9adfffe8fe900ae4469
Parkreiner <michaelsmith@coder.com>
2023-10-02 21:42:08 +0000

    chore: finish OverflowY implementation

--- site/src/components/WorkspacesButton/OverflowY.tsx
+++ site/src/components/WorkspacesButton/OverflowY.tsx
@@ -1,26 +1,32 @@
 import { type ReactNode } from "react";
 import Box from "@mui/system/Box";
+import { type SystemStyleObject } from "@mui/system";
 
 type Props = {
   children: ReactNode;
   height?: number;
   maxHeight?: number;
-  minHeight?: number;
+  sx?: SystemStyleObject;
 };
 
-export function OverflowY({
-  children,
-  height = 400,
-  minHeight = height,
-  maxHeight = height,
-}: Props) {
-  // Component should only reference min/max heights in implementation
+export function OverflowY({ children, height, maxHeight, sx }: Props) {
+  const computedHeight = height === undefined ? "100%" : `${height}px`;
+
+  // Doing Math.max check to catch cases where height is accidentally larger
+  // than maxHeight
+  const computedMaxHeight =
+    maxHeight === undefined
+      ? computedHeight
+      : `${Math.max(height ?? 0, maxHeight)}px`;
+
   return (
     <Box
       sx={{
         width: "100%",
-        height: `${height}px`,
+        height: computedHeight,
+        maxHeight: computedMaxHeight,
         overflowY: "auto",
+        ...sx,
       }}
     >
       {children}
